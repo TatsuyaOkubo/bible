2a11860240765e97e64c4d582857edf174f23415
TatsuyaOkubo <sa122moshita@gmail.com>
2020-09-11 19:18:54 +0900

    アカウントメニュー画面の作成

---
+++ app/javascript/account.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', function(){
+
+  const pullDownButton = document.getElementById("lists")
+  const pullDownParents = document.getElementById("pull-down")
+
+
+  pullDownButton.addEventListener('mouseover', function(){
+    this.setAttribute("style", "background-color:#FFBEDA;")
+    pullDownParents.setAttribute("style", "display:block;")
+  })
+
+  pullDownButton.addEventListener('mouseout', function(){
+    this.removeAttribute("style", "background-color:#FFBEDA;")
+    pullDownParents.removeAttribute("style", "display:block;")
+  })
+})
