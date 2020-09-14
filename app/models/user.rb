c98bb1aa43ff55740b1bd8a6579d25dc336eb8fb
TatsuyaOkubo <sa122moshita@gmail.com>
2020-09-14 23:19:27 +0900

    エラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+         validates :email, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+         
          with_options presence: true do
           validates :name
-      
-          VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
-          validates :email, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
       
           VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i
           validates :password, format: { with: VALID_PASSWORD_REGEX }, unless: -> { password.blank? }
