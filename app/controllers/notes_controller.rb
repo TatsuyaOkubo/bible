e8c66406810f0bc57f07af336a60970582de08d3
TatsuyaOkubo <sa122moshita@gmail.com>
2020-09-17 17:10:31 +0900

    コントローラーの設定

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,6 @@
 class NotesController < ApplicationController
+  before_action :set_note, only: [:edit, :update]
+
   def index
     @notes = Note.order('created_at DESC')
   end
@@ -19,9 +21,21 @@ class NotesController < ApplicationController
   def edit
   end
 
+  def update
+    if @note.update(note_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+  
   private
 
   def note_params
     params.require(:note).permit(:note_name, :text).merge(user_id: current_user.id)
+  end
+
+  def set_note
+    @note = Note.find(params[:id])
   end
 end
