67cc6dd7f6e9822dfb5dd7af19e706d43914ecaa
TatsuyaOkubo <sa122moshita@gmail.com>
2020-11-01 10:58:20 +0900

    googleアカウントでのログイン

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
 
          has_many :notes
 
@@ -15,4 +16,14 @@ class User < ApplicationRecord
           VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i
           validates :password, format: { with: VALID_PASSWORD_REGEX }, unless: -> { password.blank? }
         end
+
+  def self.find_or_create_for_oauth(auth)
+    find_or_create_by!(email: auth.info.email) do |user|
+      user.provider = auth.provider,
+      user.uid = auth.uid,
+      user.name = auth.info.name,
+      user.email = auth.info.email,
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
 end
